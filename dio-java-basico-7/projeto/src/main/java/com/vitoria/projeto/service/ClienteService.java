5f7bc175fbd6de6bb6caa9313191bba27b32bbf1
VitoriaSouto06 <soutovitoria06@gmail.com>
2025-02-24 19:18:20 -0300

    finalizado

---
+++ dio-java-basico-7/projeto/src/main/java/com/vitoria/projeto/service/ClienteService.java
@@ -0,0 +1,13 @@
+package com.vitoria.projeto.service;
+
+import com.vitoria.projeto.model.*;
+
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+    Cliente buscarPorId(Long id);
+    void inserir(Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
+    void deletar(Long id);
+    
+} 
